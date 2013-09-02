cc9cd9a83c185e4a9199adc00e333416d619ac28
phox.cc@gmail.com <phox.cc@gmail.com@97625c11-0b92-c080-bc93-5a255185f9f4>
2013-09-02 21:28:03 +0000

    [Issue 1] Added main code and attached a class creator to the Main-PHRAME.

--- WebContent/src/phrame.js
+++ WebContent/src/phrame.js
@@ -4,20 +4,84 @@
 * Created by:
 *    Arctic || PHOX || ArcticPHOX (aka Eric C.)
 */
-// Declared main PHRAME namespace [Arctic] 
-PHRAME = {};
-// Declared Core namespace in PHRAME namespace [Arctic] 
-PHRAME.Core = {};
 
-// PHRAME Globals [Arctic]
-PHG = {};
-// Function to write an element into the body of the document. [Arctic]
-PHG.write = function(e,b){
+(function(){
+	// Declared main PHRAME namespace
+	var PHRAME = {};
+	PHRAME.namespaces = [];
+	PHRAME.classes = [];
 	
-	// Write the element to the body
-	b.contain([e]);
+	// Function to write an element into the body of the document.
+	PHRAME.write = function(e,b){
+		
+		b.fillSize();
+		// Write the element to the body [Arctic]
+		b.contain([e]);
+		
+		return(true);
+	};
 	
-	b.fillSize();
+	// Define the class extension function.
+	// STRING name = Name of the class.
+	// OBJECT object = the class object.
+	PHRAME.Class = function(param){
+		
+		// Check for namespaces in the class name.
+		var classPath = param.name.split(".");
+		
+		// Create constructor
+		var constructFunc = function(options){
+			if(this._construct !== undefined){
+				if(typeof(options) === 'undefined'){options = {};}
+				this._construct(options);
+			}
+		};
+		
+		// Store the PHRAME namespace as the "Current Namespace" (cN).
+		// Every time we go further into the namespace, we push this out.
+		// May be a bit confusing, but the comments in the loop should help.
+		var cN = PHRAME;
+		// This will be defined as the "Current Object" (cO).
+		var cO = null;
+		// Loop through the classPath and check/create namespaces and the object.
+		for(var i=0; i < classPath.length; i++){
+			// Store the current index into a more manageable name.
+			var cP = classPath[i];
+			// If this is the last item in the classPath array,
+			// Let's now create the actual object
+			if(i === (classPath.length-1)){
+				// Now to create the object in the current working namespace.
+				cN[cP] = constructFunc;
+				// Define the prototype.
+				cO = cN[cP].prototype;
+				// Set class info
+				cO.className = cP;
+				cO.classFullName = param.name;
+				break;
+			}else{
+				// Check if the namespace doesn't exist.
+				if(typeof(cN[cP]) === 'undefined'){
+					// Add the new namespace to the current one we're on.
+					cN[cP] = {};
+				}
+				// This next line pretty much makes the next namespace our working one.
+				cN = cN[cP];
+			}
+		}
+		
+		// Add the object properties and methods
+		if(typeof(param.object) === 'object'){
+			// Place the object from the parameter into an object
+			var obj = param.object;
+			// loop through the object and add the objects.
+			for(var p in obj){
+				// Add all properties and methods to the "class"
+				cO[p] = obj[p];
+			}
+		}
+	};
 	
-	return(true);
-};
+	// Make the objects global. [Arctic]
+	window.PHRAME = PHRAME;
+	window.x = PHRAME;
+}());
